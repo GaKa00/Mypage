38884090d98425b0ac309e25a68595e072fe2da9
Gabriel Kassarp <gkassarp@gmail.com>
2024-03-22 23:15:05 +0100

    pushed up basic structure

---
+++ my-page/src/components/AboutMe.tsx
@@ -0,0 +1,28 @@
+function About(){
+    return (
+        
+      <section>
+        <h1>Hello!</h1>
+        <h2>My name is Gabriel,</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          iste? Omnis labore maiores, esse fuga dolorum doloremque quae
+          inventore nesciunt?
+        </p>
+      </section>
+      <img src="" alt="" />
+
+      <main>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio sequi excepturi quos velit sint nemo perferendis. Corrupti eos excepturi ipsum cum neque ex voluptatibus voluptates, quae laudantium tempora harum hic amet aperiam modi nisi assumenda deserunt quo dolorem labore est consequuntur similique eveniet aliquam aspernatur.
+         Id temporibus nisi exercitationem veniam!</main>
+    );
+}
+
+export default About;
+
+
+
+function TechStack(){
+    return(
+        <div className=""></div>
+    )
+}
